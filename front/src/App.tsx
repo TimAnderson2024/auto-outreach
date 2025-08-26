013a68432fd2f716bcc371cbab084ee3baf85747
Tim Anderson <trjjinva@gmail.com>
2025-08-26 02:17:39 -0400

    Implemented routing support

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ContactsPage from "./pages/ContactsPage.tsx";
 import CompanyPage from "./pages/CompanyPage.tsx";
 
 function App() {
   return (
-    <>
-      <CompanyPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/contact-view" replace />} />
+        <Route path="/contact-view" element={<ContactsPage />} />
+        <Route path="/company-view" element={<CompanyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
