1a0a882990f8aed9cc4ba9588839c2db7c20e2e7
Tim Anderson <trjjinva@gmail.com>
2025-08-26 02:22:55 -0400

    Header links functional for Contact and Company page

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,22 +1,31 @@
 import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <Navbar expand="lg" className="bg-light border-bottom">
-            <Container fluid className="mx-auto" style={{ maxWidth: "1600px" }}>
-                <Navbar.Brand href="#">Auto Outreach</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#contacts" className="text-dark">Contacts</Nav.Link>
-                        <Nav.Link href="#companies" className="text-dark">Companies</Nav.Link>
-                        <Nav.Link href="#to-do" className="text-dark">To-Do</Nav.Link>
-                        <Nav.Link href="calendar" className="text-dark">Calendar</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar expand="lg" className="bg-light border-bottom">
+      <Container fluid className="mx-auto" style={{ maxWidth: "1600px" }}>
+        <Navbar.Brand href="#">Auto Outreach</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/contact-view" className="text-dark">
+              Contacts
+            </Nav.Link>
+            <Nav.Link as={Link} to="/company-view" className="text-dark">
+              Companies
+            </Nav.Link>
+            <Nav.Link href="#to-do" className="text-dark">
+              To-Do
+            </Nav.Link>
+            <Nav.Link href="calendar" className="text-dark">
+              Calendar
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
